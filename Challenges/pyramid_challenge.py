11451ecfb47ba297d0806514722d392851f9570b
Theophile_Ishimwe <ishimtheo@gmail.com>
2022-03-04 19:26:21 +0100

    completed the Pyramid Challenge

---
+++ Challenges/pyramid_challenge.py
@@ -0,0 +1,14 @@
+
+def print_pyramid(number):
+    """
+    print a pyramid whose height depends on the input number
+    """
+    for i in range(1, number + 1):
+        print(i*"#")
+
+
+
+
+# Test
+print_pyramid(5)
+
