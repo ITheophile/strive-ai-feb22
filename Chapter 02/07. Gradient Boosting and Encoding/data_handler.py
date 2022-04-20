324bf5575935b8ea07a053d93133db36ca4c7c7d
Theophile_Ishimwe <ishimtheo@gmail.com>
2022-04-20 02:56:53 +0200

    Added a preprocessor function
    Removed preprocessing from the function `get_data`.
    Now, `get_data` only handles reading of data and splitting into training and test datasets.

--- Chapter 02/07. Gradient Boosting and Encoding/data_handler.py
+++ Chapter 02/07. Gradient Boosting and Encoding/data_handler.py
@@ -5,21 +5,37 @@ from sklearn.compose import ColumnTransformer
 
 
 def get_data(pth):
+    """
+    read data in; split into train and test datasets
+    Args(str): path to the dataset
+    Returns (tuple of pandas Dataframes and pandas Series): x_train and x_test (dfs), y_train and y_test (series)
+    """
 
+    # get data
     data = pd.read_csv(pth)
 
+    # split
     x_train, x_test, y_train, y_test = train_test_split(data[:,:-1], data[:,-1], test_size=0.2, random_state = 0)
-    
-    oe = OrdinalEncoder(handle_unknown= 'use_encoded_value', unknown_value = -1)
-    scaler = StandardScaler()
-    
-    ct = ColumnTransformer([('ordinal', oe, ['sex', 'smoker', 'region']), 
-                            ('scaler', scaler, ['age', 'bmi'])], 
-                            remainder='passthrough')
-
-    x_train = ct.fit_transform(x_train)
-    x_test = ct.transform(x_test)
     
     return x_train, x_test, y_train, y_test
 
 
+
+def preprocessor(numerical_features, categorical_features):
+    """
+    Set up transformations (ordinal encoding and standardization) to apply on selected features.
+        Ordinal encoding is apply to `categorical_features` and standardization on `numerical_features`
+
+    Args (list): List of column names to apply transformations on.
+    Outputs (sklearn ColumnTransformer): A combination of transformers.
+
+    """
+    
+    oe = OrdinalEncoder(handle_unknown= 'use_encoded_value', unknown_value = -1)
+    scaler = StandardScaler()
+        
+    ct = ColumnTransformer([('ordinal', oe, categorical_features ), 
+                                ('scaler', scaler, numerical_features )], 
+                                remainder='passthrough')
+
+    return ct
