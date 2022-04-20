ed5efc3597538c82212e5a8aa4e391429e639149
Theophile_Ishimwe <ishimtheo@gmail.com>
2022-04-20 06:28:19 +0200

    added CLI to retrieve user's data and make prediction

--- Chapter 02/07. Gradient Boosting and Encoding/predict.py
+++ Chapter 02/07. Gradient Boosting and Encoding/predict.py
@@ -1,28 +1,34 @@
-from train import best_model
+import joblib
 import pandas as pd
-model = best_model
 
+# Loading model and preprocessor
+ct = joblib.load('preprocessor.joblib')
+model = joblib.load('model.joblib')
+
+# prediction using user data through command line interface
 while True:
 
     age = int(input("How old are you? \n"))
-    sex = input("Are you male or Female? \n")
+    sex = input("Are you male or female? \n")
     bmi = float(input("What is your bmi? \n"))
     children = int(input("How many children do you have? \n"))
-    region = input('From which of the following regions are you coming from (southwest, southeast, northwest, northeast)?')
+    region = input("""From which of the following regions are you coming from ?
+    (southwest, southeast, northwest, northeast) \n""")
     smoker = input("Do you smoke? \n")
 
-    user_data = pd.DataFrame({'age': age, 'sex': sex, 
-                              'bmi':bmi, 'children':children, 
-                              'smoker':smoker, 'region':region})
     
-    '''
-    Preprocess
-    predict
-    
-    '''
+    user_data = pd.DataFrame([[age, sex, bmi, children, smoker, region]], 
+    columns= ['age', 'sex', 'bmi', 'children', 'smoker', 'region'])
     
     
+    #Preprocess
+    user_data_transfomed = ct.transform(user_data)
+
+    #predict
+    pred = model.predict(user_data_transfomed)[0]
     
-    print("You are too fucked up 1 milly")
-    # print(model.predict(user_data))
+    
+    # display prediction
+    print(f"your predicted insurance charges amount to {round(pred, 2)}")
+
     
