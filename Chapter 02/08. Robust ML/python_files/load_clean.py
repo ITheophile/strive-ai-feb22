7e3face44a11687708222adaa1a7897a7fd8402f
Theophile_Ishimwe <ishimtheo@gmail.com>
2022-04-22 08:17:42 +0200

    Added `map_title` method in `titanic_utilities.py`
    Also used `map_title` method to further clean train and test sets `load_clean.py`

--- Chapter 02/08. Robust ML/python_files/load_clean.py
+++ Chapter 02/08. Robust ML/python_files/load_clean.py
@@ -1,12 +1,53 @@
-from titanic_functions import load
+from titanic_utilities import load, CleanData
 
 
-# Loading the titanic's train and test sets
+# 1. Loading the titanic's train and test sets
 pth_train = '../titanic/train.csv'
 pth_test = '../titanic/test.csv'
 
 df = load(pth_train)
 df_test = load(pth_test)
 
-print(df.sample(10))
-print(df_test.sample(10))
+
+# print(df.sample(10))
+# print(df_test.sample(10))
+
+# 2. Cleaning
+# Add Title column
+train = CleanData(df)
+test = CleanData(df_test)
+
+df = train.get_title('Name', 'Title').data
+
+df_test = test.get_title('Name', 'Title').data
+
+
+# Map titles to broader categories
+title_dictionary = {
+    "Capt": "Officer",
+    "Col": "Officer",
+    "Major": "Officer",
+    "Jonkheer": "Royalty",
+    "Don": "Royalty",
+    "Sir" : "Royalty",
+    "Dr": "Officer",
+    "Rev": "Officer",
+    "the Countess":"Royalty",
+    "Mme": "Mrs",
+    "Mlle": "Miss",
+    "Ms": "Mrs",
+    "Mr" : "Mr",
+    "Mrs" : "Mrs",
+    "Miss" : "Miss",
+    "Master" : "Master",
+    "Lady" : "Royalty"
+}
+
+df = train.map_title(title_dictionary).data
+df_test = test.map_title(title_dictionary).data
+
+
+
+
+
+
--- Chapter 02/08. Robust ML/python_files/titanic_utilities.py
+++ Chapter 02/08. Robust ML/python_files/titanic_utilities.py
@@ -20,10 +20,40 @@ def load(pth):
 
 
 
-class Cleaner():
+class CleanData():
     """
     Class for handling the cleaning of data
     """
     def __init__(self, data):
         self.data = data
+        self.title_column = None
+
+
+    def get_title(self, name_column, title_column):
+        """
+        Extract the title from Passenger's name and create a new column in `self.data` as `name_column`.
+        Args:
+            name_column(str): The name of column containing passengers' names.
+            title_column(str): The name of column where the title will be stored
+        Returns (self):
+        """
+        get_Title_from_Name = lambda x: x.split(',')[1].split('.')[0].strip()
+        self.data[title_column] = self.data[name_column].apply(get_Title_from_Name)
+        self.title_column = title_column
+        
+        return self
+
+    def map_title(self, mapper):
+        """
+        Create broader categories of titles based on a specified `mapper` and overwrite the `title_column`
+        Args:
+            title_column(str): The name of column containing passengers' titles
+            mapper(dict): A dictionary of old titles (keys) and new titles (values)
+
+        Returns (self):
+        """
+
+        self.data[self.title_column] = self.data[self.title_column].map(mapper)
+        return self
+
         
