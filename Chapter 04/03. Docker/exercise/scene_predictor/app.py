368ef5dae52f00d2df5a5069fa5af265358ccb3f
Theophile_Ishimwe <ishimtheo@gmail.com>
2022-07-19 20:34:19 +0200

    completed exercise

--- Chapter 04/03. Docker/exercise/scene_predictor/app.py
+++ Chapter 04/03. Docker/exercise/scene_predictor/app.py
@@ -1,6 +1,5 @@
 from utils import store_uploaded_image, clean_upload_folder
 from model.model_setup import model
-
 from model.prediction import make_prediction
 
 import os
@@ -16,12 +15,16 @@ app = Flask(__name__)
 @app.route('/', methods = ['GET','POST'])
 def index():
     
-    # Store the image and return its path
+    # start by removing any previous image 
+    # -> no need to accumulate previously loaded images
+    clean_upload_folder()
+
+    # Store the actual image and return its path
     img_pth = store_uploaded_image()
 
     # test output
     if img_pth:
-
+        
         # load image
         img = Image.open(img_pth)
 
@@ -37,7 +40,14 @@ def index():
     
         pred = make_prediction(model, img, classes)
 
-        return render_template('index.html', prediction = pred.upper())
+
+        # image path for displaying in the bootstrap card
+        if img_pth.find('\\') > 0: # backslash found
+            new_file_path = img_pth.replace('\\', '/').replace('static/', '')
+        else: # forward slash
+            new_file_path = img_pth.replace('static/', '')
+
+        return render_template('index.html', prediction = pred.upper(), img_pth = new_file_path)
 
     else:
         return render_template('index.html')
@@ -45,7 +55,8 @@ def index():
 
     
     
-clean_upload_folder()
+# clean_upload_folder()
+
 
 
 if __name__ == '__main__':
