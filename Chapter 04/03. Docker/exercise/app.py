95cf652608cb07a8afd12140515c3d5d0765e0a0
Theophile_Ishimwe <ishimtheo@gmail.com>
2022-07-19 13:21:31 +0200

    created functions to save and deleted uploaded images

--- Chapter 04/03. Docker/exercise/app.py
+++ Chapter 04/03. Docker/exercise/app.py
@@ -1,14 +1,22 @@
+from utils import store_uploaded_image, clean_upload_folder
 from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
+
+@app.route('/', methods = ['GET','POST'])
 def index():
+    
+    output = store_uploaded_image()
+    if output:
+        
+        return render_template('test.html', test = output)
     return render_template('index.html')
 
-
-
+print(clean_upload_folder())
+    
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run('0.0.0.0', debug=True)
---
+++ Chapter 04/03. Docker/exercise/utils.py
@@ -0,0 +1,34 @@
+import os
+from flask import request, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'uploaded_images'
+
+def store_uploaded_image():
+    '''
+    Stores the uploaded images into the UPLOAD_FOLDER 
+    within the static folder
+    '''
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            file_name = secure_filename(file.filename)
+            file_path = os.path.join('static', UPLOAD_FOLDER, file_name)
+            file.save(file_path)
+            return file_path
+        else:
+            return 'No file found...Did you uploaded an image?'
+
+
+
+
+def clean_upload_folder():
+    """
+    remove images from UPLOAD_FOLDER
+    """
+    upload_folder_path = os.path.join('static', UPLOAD_FOLDER)
+    image_names = os.listdir(upload_folder_path)
+    for image in image_names:
+        img_pth = os.path.join(upload_folder_path, image)
+        os.remove(img_pth)
+
